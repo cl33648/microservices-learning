b7a1809b8f017c26bcab6e886d3cd326821e4a72
Conrad Lee <conradlee@Conrads-MacBook-Pro.local>
2022-03-13 19:32:22 -0500

    add distributed tracing system zipkin

--- clients/src/main/java/com/microservices/clients/notification/NotificationRequest.java
+++ clients/src/main/java/com/microservices/clients/notification/NotificationRequest.java
@@ -1,8 +1,10 @@
 package com.microservices.clients.notification;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 public class NotificationRequest {
     public Integer toCustomerId;
     public String toCustomerEmail;
